13af6d233732f3551d9282292c2844501fe2cb01
abhtri2009 <abhtri2009@gmail.com>
2019-11-03 01:10:33 +0530

    dashboard initial design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StocklistComponent } from './stocklist/stocklist.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GraphComponent } from './graph/graph.component';
+import { WeatherformComponent } from './weatherform/weatherform.component';
+import { LivechatComponent } from './livechat/livechat.component';
+import { ChartsModule } from 'ng2-charts';
+import { ReactiveFormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StocklistComponent,
+    GraphComponent,
+    WeatherformComponent,
+    LivechatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ChartsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
